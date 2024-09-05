a48add43817f21abcf71bedcb42993c33c0559e3
kcnyj <kcnyj@boomco.org>
2024-09-05 14:15:42 +0900

    로그인, 회원가입, 비밀번호찾기 완료

---
+++ src/pages/login/Password.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PasswordForm from './PasswordForm'
+
+const Password = () => {
+    return (
+        <div>
+            <PasswordForm />
+        </div>
+    )
+}
+
+export default Password
